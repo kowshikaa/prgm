a0feabb36873138d124068ae98c79c4d8e2ab7ff
kowshikaa <kowshimano@gmail.com>
2016-07-10 12:52:40 +0530

    finish

---
+++ README.c
@@ -0,0 +1,28 @@
+# prgm
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+int i,j;
+char str[10][50],temp[50];
+printf("Enter words:");
+for(i=0;i<10;++i)
+{
+ gets(str[i]);
+ for(i=0;i<9;++i)
+ {
+ for(j=i+1;j<10 ;++j)
+ {
+ if(strcmp(str[i],str[j])>0)
+ {
+ strcpy(temp,str[i]);
+ strcpy(str[i],str[j]);
+ strcpy(str[j],temp);
+ }
+ }}}
+for(i=0;i<10;++i)
+{
+puts(str[i]);
+}
+return 0;
+}
